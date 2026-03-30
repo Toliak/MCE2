ff45893b285c2041eda331bcbd7ea2dbc08ec25a
toliak <toliakpurple@gmail.com>
2026-03-30 20:54:10 +0300

    Improve help modal

--- configapp/cmd/mce/ui/modals.go
+++ configapp/cmd/mce/ui/modals.go
@@ -2,6 +2,8 @@ package ui
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
@@ -9,13 +11,94 @@ import (
 )
 
 // NewHelpModal creates a help modal
-func NewHelpModal(state *UIState, app *tview.Application, tegnGeneral *tb.TegnGeneral, closer func()) *tview.Flex {
+func NewHelpModal(state *UIState, app *tview.Application, tegnGeneralPtr *tb.TegnGeneral, availability *tb.TegnAvailability, closer func()) *tview.Flex {
 	var helpText string
 
-	if tegnGeneral == nil {
+	if tegnGeneralPtr == nil {
 		helpText = `No help available`
 	} else {
-		helpText = (*tegnGeneral).GetDescription()
+		tegnGeneral := *tegnGeneralPtr
+
+		var sb strings.Builder
+		sb.WriteString("[::b]Description[::-]\n")
+		sb.WriteString(tegnGeneral.GetDescription())
+
+		sb.WriteString("\n\n[::b]Availability[::-]\n")
+		sb.WriteString("CPU Architectures: ")
+		cpuArchList := tegnGeneral.GetAvailableCPUArch()
+		if cpuArchList == nil {
+			sb.WriteString("any\n")
+		} else {
+			if len(*cpuArchList) == 0 {
+				sb.WriteString("nothing! (error?)\n")
+			} else {
+				sb.WriteString("\n")
+				for _, arch := range *cpuArchList {
+					sb.WriteString("- ")
+					sb.WriteString(arch.String())
+					sb.WriteString("\n")
+				}
+			}
+		}
+
+		sb.WriteString("OS Types: ")
+		osTypeList := tegnGeneral.GetAvailableOsType()
+		if osTypeList == nil {
+			sb.WriteString("any\n")
+		} else {
+			if len(*osTypeList) == 0 {
+				sb.WriteString("nothing! (error?)\n")
+			} else {
+				sb.WriteString("\n")
+				for _, osType := range *osTypeList {
+					sb.WriteString("- ")
+					sb.WriteString(osType.String())
+					sb.WriteString("\n")
+				}
+			}
+		}
+
+		sb.WriteString("\n")
+		if availability == nil {
+			sb.WriteString("No detailed availability info\n")
+		} else {
+			sb.WriteString("Available: ")
+			if availability.Available {
+				sb.WriteString("yes\n")
+			} else {
+				sb.WriteString("no\n")
+				sb.WriteString("Reason: ")
+				sb.WriteString(availability.Reason)
+				sb.WriteString("\n")
+			}
+			
+		}
+		
+		// tegnGeneral.
+		if tegn, ok := tegnGeneral.(tb.Tegn); ok {
+			sb.WriteString("\n[::b]Tegn Features[::-]\n")
+			featureList := tegn.GetFeatures()
+			if len(featureList) == 0 {
+				sb.WriteString("no features (error?)\n")
+			} else {
+				for ft, _ := range featureList {
+					sb.WriteString("- ")
+					sb.WriteString(string(ft))
+					sb.WriteString("\n")
+				}
+			}
+
+			sb.WriteString("\n[::b]Tegn Already installed[::-]: ")
+			if state.InstalledCache[tegnGeneral.GetID()] {
+				sb.WriteString("yes\n")
+			} else {
+				sb.WriteString("no\n")
+			}
+		} else if _, ok := tegnGeneral.(tb.Tegnsett); ok {
+			// No additional info
+		}
+
+		helpText = sb.String()
 	}
 	
 	textView := tview.NewTextView().
@@ -46,7 +129,7 @@ func NewHelpModal(state *UIState, app *tview.Application, tegnGeneral *tb.TegnGe
 	form.SetTitle("Help").
 		SetBackgroundColor(tcell.ColorDarkBlue).
 		SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-			if event.Key() == tcell.KeyEscape {
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyEnter {
 				closer()
 				return nil
 			}
